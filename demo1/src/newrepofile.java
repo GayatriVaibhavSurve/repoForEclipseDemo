d0feb4890aea8753fd6220938d423b9fa21ee943
Gayatri.surve <gayatrisurve91@gmail.com>
2021-05-24 21:36:46 +0530

    this is branch24 file

--- demo1/src/newrepofile.java
+++ demo1/src/newrepofile.java
@@ -3,7 +3,7 @@ public class newrepofile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+          System.out.println("this is new file in branch24");
 	}
 
 }
