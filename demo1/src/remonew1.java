501542c253dbf829c43dde111c259d2cf32513d4
Gayatri.surve <gayatrisurve91@gmail.com>
2021-05-24 20:02:23 +0530

    want to commit file

---
+++ demo1/src/remonew1.java
@@ -0,0 +1,9 @@
+
+public class remonew1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I want to push a branch in remote repository..");
+	}
+
+}
